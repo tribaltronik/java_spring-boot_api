636c62b4b75f19851e92a1d8c7c5b744d8c775ac
Tiago Ricardo <ricardo@unicc.org>
2026-03-06 16:19:40 +0000

    Clean repo

--- target/classes/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id, username, password, roles) VALUES (1, 'user', '$2a$10$b3.q/S0YjD3.t.L0.N0.T0.H0.K0.L0.P0.M0.N0.O0.P0.S0.Q0.R0.S0.T0.U0.V0.W0.X0.Y0.Z0', 'ROLE_USER');
--- Password for 'user' is 'password' (bcrypt encoded)
